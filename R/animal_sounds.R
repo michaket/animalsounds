d9c7a491a9593fab92330932be4525d8cf8f54ad
MKE <michael.ketzer@unibas.ch>
2025-11-26 15:15:12 +0100

    Improves input validation with `cli` and `rlang`
    Replaces `stopifnot` with `cli::cli_abort` and `rlang::is_character` for enhanced error messaging and input validation in the `animal_sounds` function.
    This change provides more informative and user-friendly error messages when the `animal` or `sound` arguments are not single strings.
    Additionally, adds missing imports to the DESCRIPTION and NAMESPACE files.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -13,8 +13,13 @@
 #' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound){
 
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+  if (!rlang::is_character(animal, n = 1)) {
+    cli::cli_abort("`animal` must be a single string")
+  }
+
+  if (!rlang::is_character(sound, n = 1)) {
+    cli::cli_abort("`sound` must be a single string")
+  }
 
   paste0("The ", animal, " goes ", sound, "!")
 
