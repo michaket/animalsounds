dd936f0ef800a55530c5b96b18291cb748855f23
MKE <michael.ketzer@unibas.ch>
2025-11-26 14:40:31 +0100

    Updates documentation to reflect `paste0()` usage
    Updates the documentation for the `animal_sounds` function to clarify that it uses `paste0()` internally.
    This provides users with more clarity on the function's implementation.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,7 +1,7 @@
 #' Print the Sound an Animal Makes
 #'
 #' This function takes an animal and the sound it makes,
-#' and returns a formatted string.
+#' and returns a formatted string by creating a wrapper around [paste0()].
 #'
 #' @param animal A character vector of length 1
 #' @param sound A character vector of length 1
