e23e4b8a141bbeb02410708c8bc0fa11dfc90d88
MKE <michael.ketzer@unibas.ch>
2025-11-26 14:23:00 +0100

    Adds animal sounds function
    Adds a function that returns a formatted string
    combining an animal and the sound it makes.
    
    Also adds relevant documentation and exports the function.

---
+++ R/animal_sounds.R
@@ -0,0 +1,21 @@
+#' Print the Sound an Animal Makes
+#'
+#' This function takes an animal and the sound it makes,
+#' and returns a formatted string.
+#'
+#' @param animal A character vector of length 1
+#' @param sound A character vector of length 1
+#'
+#' @returns A character that combines the animal name and sound.
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+animal_sounds <- function(animal, sound){
+
+  stopifnot(is.character(animal) & length(animal) == 1)
+  stopifnot(is.character(sound) & length(sound) == 1)
+
+  paste0("The ", animal, " goes ", sound, "!")
+
+}
