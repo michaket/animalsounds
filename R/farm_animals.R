221884c15732df7f56a07c1d6f2a94c808b3eb61
MKE <michael.ketzer@unibas.ch>
2025-11-27 12:05:05 +0100

    Adds farm animal sounds dataset
    Introduces a new dataset containing farm animals and their corresponding sounds.
    
    Includes the data in `data/`, raw data in `data-raw/`,
    documentation, and adds `LazyData: true` to the DESCRIPTION file
    to enable lazy loading of the dataset.
    
    Also, excludes the data-raw directory from the build process.

---
+++ R/farm_animals.R
@@ -0,0 +1,13 @@
+#' Farm Animal Sounds
+#'
+#' Farm animals and the sounds they make.
+#'
+#' @format A data frame with five rows and two columns
+#' \describe{
+#'  \item{animal}{Type of farm animal}
+#'  \item{sound}{Sound made by the animal}
+#'  }
+
+
+
+"farm_animals"
---
+++ data-raw/farm_animals.R
@@ -0,0 +1,11 @@
+## code to prepare `farm_animals` dataset goes here
+
+animal <- c("cow", "horse", "duck", "sheep", "pig")
+sound <- c("moo", "neigh", "quack", "baa", "oink")
+
+farm_animals <- data.frame(
+  animal = animal,
+  sound = sound)
+
+
+usethis::use_data(farm_animals, overwrite = TRUE)
